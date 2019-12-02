c8c1b92b36329705f674d1497bbc47c1ca245217
Vinicius Pucci <vinicius.pucci@gmail.com>
2019-12-02 18:49:07 -0300

    Updated title

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
   require('php/connect.php');
 
   if(!isset($_SESSION)) session_start();
+
+  $title = "PimpSYS";
 ?>
 
 <!--
@@ -25,9 +27,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>
-    Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
-  </title>
+  <title><?= $title ?></title>
   <!-- Favicon -->
   <link href="./assets/img/brand/favicon.png" rel="icon" type="image/png">
   <!-- Fonts -->
