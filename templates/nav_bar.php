417c3db4730fb58ae145fc8d538cd310e0cce68f
Vinicius Pucci <vinicius.pucci@gmail.com>
2019-12-03 14:49:50 -0300

    Viwers organization

--- templates/nav_bar.php
+++ templates/nav_bar.php
@@ -1,5 +1,6 @@
 <div class="container-fluid">
         <!-- Brand -->
+        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="index.php">User Profile</a>
         <!-- Form -->
         <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
           <div class="form-group mb-0">
