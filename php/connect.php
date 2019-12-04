b71df6c07caf3f77af280238f4cd1cee9a0b176e
Vinicius Pucci <vinicius.pucci@gmail.com>
2019-12-04 12:35:46 -0300

    Add sweetalert and first cad

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
-    $host = '';
-    $user = '';
+    $host = 'localhost';
+    $user = 'root';
     $pw = '';
-    $schema = '';
+    $schema = 'pimpsys';
 
     $con = mysqli_connect($host, $user, $pw, $schema) or die('Falha ao conectar ao banco de dados');
 ?>
